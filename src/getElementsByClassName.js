554ab3e51f6c7579c149eaa4cae34eb29006700e
Alex Cai <yhcai@ucdavis.edu>
2020-01-13 16:49:45 -0800

    finished

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+
+    var docBody = document.body;
+    var output = [];
+    var findElementByClass = function(element){
+      if(element.classList && element.classList.contains(className)){
+        output.push(element);}
+        if(element.childNodes){
+          for(var i = 0; i < element.childNodes.length; i++){
+            output.concat(findElementByClass(element.childNodes[i]));
+          }
+        }
+        return output;
+      };
+      return findElementByClass(docBody);
+  };
+  
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,54 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //var string = '';
+
+
+  if (obj === null){
+    return "null";
+  }
+
+  else if(typeof obj ==="number"){
+    return obj.toString();
+  }
+
+  else if(typeof obj === "boolean"){
+    if(obj){
+      return "true";
+    } else{
+      return "false";
+    }
+  }
+
+  else if(typeof obj === "string"){
+    return '"' + obj + '"';
+  }
+  else if(Array.isArray(obj)){
+   var stringArr = [];
+
+   obj.forEach(function(element){
+     stringArr.push(stringifyJSON(element));
+   })
+   return "[" + stringArr.toString() + "]";
+ }
+
+  else if(typeof obj === "function"){
+    return "";
+  }
+
+  else if(typeof obj === "object"){
+  var stringObj = "";
+  var objKeys = Object.keys(obj);
+  objKeys.forEach(function(k){
+    if(typeof obj[k] === "function" || typeof obj[k] === "undefined"){
+      return "";
+    }
+    else{stringObj += stringifyJSON(k) + ':' + stringifyJSON(obj[k]) + ',';
+    }
+  })
+  return "{" + stringObj.slice(0, stringObj.length - 1) + "}"
+  }
+
+ // return obj.toString();
+  
 };
